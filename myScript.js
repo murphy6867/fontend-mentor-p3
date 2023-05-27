751a4839629ee127fd1b4c945771320f92c14751
Sonsawan <sonsawan.n1994@gmail.com>
2023-05-27 19:23:51 +0700

    fix .js code to calculate date result.

--- myScript.js
+++ myScript.js
@@ -1,23 +1,31 @@
-
 function getDate() {
     // Get user input
     const _day = parseInt(document.getElementById("day").value);
     const _month = parseInt(document.getElementById("month").value);
     const _year = parseInt(document.getElementById("year").value);
-
-    const _currentDate = new Date(); // Get the Current date.
+  
+    const _currentDate = new Date(); // Get the current date
     const _userBirthDate = new Date(_year, _month - 1, _day); // Create a Date object for the user's birthday
-
-    // Calculate the difference in milliseconds between the current date and the user's birthday
-    const _timeDiff = _currentDate.getTime() - _userBirthDate.getTime();
-
-    // Calculate the total years, months, and days
-    const years = Math.floor(_timeDiff / (1000 * 60 * 60 * 24 * 365.25));
-    const months = Math.floor((_timeDiff % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44));
-    const days = Math.floor((_timeDiff % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24));
-
-    // Out put of Date
+  
+    // Calculate the difference in years, months, and days
+    let years = _currentDate.getFullYear() - _userBirthDate.getFullYear();
+    let months = _currentDate.getMonth() - _userBirthDate.getMonth();
+    let days = _currentDate.getDate() - _userBirthDate.getDate();
+  
+    // Adjust for negative values or incomplete months
+    if (months < 0 || (months === 0 && days < 0)) {
+      years--;
+      months += 12;
+    }
+    if (days < 0) {
+      const prevMonthDate = new Date(_currentDate.getFullYear(), _currentDate.getMonth() - 1, 0);
+      days += prevMonthDate.getDate();
+      months--;
+    }
+  
+    // Output the age
     document.getElementById("ResultYear").textContent = years;
     document.getElementById("ResultMonth").textContent = months;
     document.getElementById("ResultDay").textContent = days;
-}
+  }
+  
